35db0e145fd88836455ad9f5866a595efe78e70e
Ditto25 <115644150+Ditto25@users.noreply.github.com>
2025-11-24 20:01:12 +0100

    fix database

--- Pages/1_Elhub_data.py
+++ Pages/1_Elhub_data.py
@@ -33,7 +33,7 @@ def load_data():
     """Load and process data from MongoDB"""
     client = get_mongo_client()
     
-    database = client['Database'] 
+    database = client['database'] 
     collection = database['data']
     
     # Fetch all documents from MongoDB
